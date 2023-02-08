d9d041b1ba7781f737ce0ba894df7eb5bafa9b69
nicopina <pinanicolasagustin@gmail.com>
2023-02-07 23:40:27 -0300

    initial css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-        <div className="gradiant__bg">
+        <div className="gradient__bg">
             <Navbar />
             <Header />
          </div>

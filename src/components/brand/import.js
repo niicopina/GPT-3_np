2c70872cbddde6d2bb04210462ec17517a6741fa
nicopina <pinanicolasagustin@gmail.com>
2023-02-11 19:26:07 -0300

    brand

---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.png'
+import slack from '../../assets/google.png'
+import atlassian from '../../assets/google.png'
+import dropbox from '../../assets/google.png'
+import shopify from '../../assets/google.png'
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+}

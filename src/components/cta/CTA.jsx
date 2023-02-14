a27e39676b54aa78221874605edf9f45794adde4
nicopina <pinanicolasagustin@gmail.com>
2023-02-14 20:23:49 -0300

    cta section

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './cta.css'
+
 const CTA = () => {
   return (
-    <div>Cta</div>
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request early acces to get Started</p>
+        <h3>Register today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -9,7 +9,7 @@ const Possibility = () => {
       </div>
       <div className="gpt3__possibility-content">
         <h4>Request early acces to get started</h4>
-        <h1 className="gradient__text">The possibilities are beyon our imagination</h1>
+        <h1 className="gradient__text">The possibilities are beyond our imagination</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <h4>Request Early Access to Get Started</h4>
       </div>

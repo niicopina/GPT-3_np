7eb644e8d2bd8e4800628d76b7a60b00ec3188b3
nicopina <pinanicolasagustin@gmail.com>
2023-02-14 21:40:07 -0300

    commit

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -8,7 +8,7 @@ const Possibility = () => {
         <img src={possibilityImage} alt="possibility"/>
       </div>
       <div className="gpt3__possibility-content">
-        <h4>Request early acces to get started</h4>
+        <h4>Request early access to get started</h4>
         <h1 className="gradient__text">The possibilities are beyond our imagination</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
         <h4>Request Early Access to Get Started</h4>
